4225483961491317c29145ce1d250131c6804562
yariv <yariv.tal@gmail.com>
2024-08-09 02:00:38 +0300

    update attack spoilers

---
+++ csharp/recipes_dotnet8_angular17/recipes-api/Spoliers/Attacking_protecting/Snippets/CookieAppend.cs
@@ -0,0 +1,8 @@
+﻿CookieOptions options = new CookieOptions
+{
+    Expires = DateTime.Now.AddDays(1),
+    HttpOnly = true,
+    //Secure = true
+};
+
+Response.Cookies.Append("user", "JohnDoe", options);
