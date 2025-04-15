6b7f9492a9217ab080954721b724ca718149326f
securefromscratch <securefromscratch@gmail.com>
2025-04-15 20:07:59 +0300

    custom errors in prod

--- csharp/dotnetcore_quiz/Program.cs
+++ csharp/dotnetcore_quiz/Program.cs
@@ -11,8 +11,23 @@ builder.Services.AddSingleton<SecureFromScratch.Quiz.Services.MultipleChoiceQues
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage(); // shows detailed errors
+}
+else
+{    
+    app.UseExceptionHandler("/error"); // generic error endpoint
+    app.UseHsts();    
+}
+
+app.Map("/error", () => Results.Problem("An unexpected error occurred."));
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
 app.MapControllers();
+
+
+
+
 app.Run();
