bfe8ce1a6330a61e6fff8a79405a8e129ddff3f0
SecureFromScratch <117460248+SecureFromScratch@users.noreply.github.com>
2025-05-01 16:42:04 +0300

    Update app-routing.module.ts

--- csharp/recipes_dotnet8_angular17/recipes-app/src/app/app-routing.module.ts
+++ csharp/recipes_dotnet8_angular17/recipes-app/src/app/app-routing.module.ts
@@ -4,33 +4,13 @@ import { RecipesCatalogComponent } from './recipes-catalog/recipes-catalog.compo
 import { AddRecipeComponent } from './add-recipe/add-recipe.component';
 import { LoginComponent } from './login/login.component';
 import { authGuard } from './auth.guard';
-
-
-/*
-const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', component: RecipesCatalogComponent },
-  { path: 'add-recipe', component: AddRecipeComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }
-];
-
-
-const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'recipes', component: RecipesCatalogComponent, canActivate: [authGuard] },
-  { path: 'add', component: AddRecipeComponent, canActivate: [authGuard] },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }
-];
-*/
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }
+  { path: 'login', component: LoginComponent },  
+  { path: 'dashboard', component: DashboardComponent },  
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
