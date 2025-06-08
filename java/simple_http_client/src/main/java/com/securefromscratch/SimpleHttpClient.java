e3009cdba64dcc06662cf0eca2b7a9aa342967d0
yariv <yariv.tal@gmail.com>
2025-06-08 17:35:16 +0300

    updated

--- java/simple_http_client/src/main/java/com/securefromscratch/SimpleHttpClient.java
+++ java/simple_http_client/src/main/java/com/securefromscratch/SimpleHttpClient.java
@@ -5,23 +5,24 @@ import java.net.*;
 
 public class SimpleHttpClient {
     public static void main(String[] args) {
-        // ========CONTROL HOSTNAME, PORT, PATH HERE=========
+        // ========[PARTIALLY IMPLEMENTED] CONTROL HOSTNAME, PORT, PATH HERE=========
         String hostname = "www.securefromscratch.com";
         int port = 80;
         String path = "/simple.html";
+        String bodyOutputFile = null;
         // ========END OF CONTROL HOSTNAME, PORT, PATH=========
 
         try (Socket socket = new Socket(hostname, port);
             PrintWriter out = new PrintWriter(socket.getOutputStream(), false);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
-            // ========ADD HEADERS, BODY HERE=========
+            // ========MODIFY CODE HERE=========
             // Send an HTTP GET request
-            out.write("GET " + path + " HTTP/1.1\r\n");
-            out.write("Host: " + hostname + "\r\n");
+            out.write("GET /simple.html HTTP/1.1\r\n");
+            out.write("Host: www.securefromscratch.com\r\n");
             out.write("Connection: close\r\n");
             out.write("\r\n");
-            // ========END OF ADD HEADERS, BODY=========
+            // ========END OF MODIFY CODE AREA=========
 
             out.flush(); // ANSWER: What does this do? Why is it needed?
 
